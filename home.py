6be38f944d5934be6caaefbbd653ba597208f2a6
Patcharida28 <patcharidachoomchoo@gmail.com>
2024-03-28 14:47:02 +0700

    Update home.py

--- home.py
+++ home.py
@@ -11,7 +11,7 @@ def load_lottieurl(url: str):
 
 lottie_url_hello = "https://lottie.host/377a24ee-4e19-48c9-8a2f-fcada962c70a/oax9riei3m.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
-st_lottie(lottie_hello.key="hello")
+st_lottie(lottie_hello,key="hello")
 
 st.page_link("home.py", label="หน้าแรก", icon="🏠")
 
