4738c3c5f6ab7194666b21b2a0f8a319e587f812
Patcharida28 <patcharidachoomchoo@gmail.com>
2024-03-28 15:21:16 +0700

    Update Chart.py

--- pages/Chart.py
+++ pages/Chart.py
@@ -1,18 +1,39 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Website Developing using Python")
-st.header("🌶️Website Developing using Python🌶️")
-
-st.subheader("🍔Patcharida Choomchoo🍔")
-st.image('patch.jpg')
+#st.title("Website Developing using Python")
+#st.header("🌶️Website Developing using Python🌶️")
 
 dt=pd.read_csv('./data/Raisin.csv')
 
-st.subheader("ข้อมูลดอกไม้ Iris")
+st.subheader("ข้อมูลRaisin")
 st.write(dt.head(10))
 #Index(['sepal.length', 'sepal.width', 'petal.length', 'petal.width',
 #       'variety'],
+
+st.subheader("สถิติข้อมูลRaisin")
+st.write('ผลรวม')
+cl1,cl2,cl3,cl4=st.columns(4)
+cl1.write(dt['Area'].sum())
+cl2.write(dt['ConvexArea'].sum())
+cl3.write(dt['Extent'].sum())
+cl4.write(dt['Perimeter'].sum())
+
+st.write("กราฟแท่ง")
+a=dt['Area'].sum()
+b=dt['ConvexArea'].sum()
+c=dt['Extent'].sum()
+d=dt['Perimeter'].sum()
+dx=[a,b,c,d]
+cx=pd.DataFrame(dx,index=["Area", "ConvexArea", "Extent","Perimeter"])
+st.bar_chart(cx)
+
+st.write('ค่าเฉลี่ย')
+cl11,cl12,cl13,cl14=st.columns(4)
+cl11.write(dt['Area'].mean())
+cl12.write(dt['ConvexArea'].mean())
+cl13.write(dt['Extent'].mean())
+cl14.write(dt['Perimeter'].mean())
 
 st.write("Area_Chart")
 a=dt['Area'].mean()
