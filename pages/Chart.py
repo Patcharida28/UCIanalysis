3f557782de6360fd1ed2b408ac0ac51d52326cfc
Patcharida28 <patcharidachoomchoo@gmail.com>
2024-03-28 15:18:27 +0700

    Update Chart.py

--- pages/Chart.py
+++ pages/Chart.py
@@ -4,36 +4,17 @@ import pandas as pd
 st.title("Website Developing using Python")
 st.header("🌶️Website Developing using Python🌶️")
 
-dt=pd.read_csv('./data/Raisin.csv')
+st.subheader("🍔Patcharida Choomchoo🍔")
+st.image('patch.jpg')
 
-st.subheader("ข้อมูลRaisin")
+dt=pd.read_csv('./data/iris.csv')
+
+dt=pd.read_csv('./data/iris.csv')
+
+st.subheader("ข้อมูลดอกไม้ Iris")
 st.write(dt.head(10))
 #Index(['sepal.length', 'sepal.width', 'petal.length', 'petal.width',
 #       'variety'],
-
-st.subheader("สถิติข้อมูลRaisin")
-st.write('ผลรวม')
-cl1,cl2,cl3,cl4=st.columns(4)
-cl1.write(dt['Area'].sum())
-cl2.write(dt['ConvexArea'].sum())
-cl3.write(dt['Extent'].sum())
-cl4.write(dt['Perimeter'].sum())
-
-st.write("กราฟแท่ง")
-a=dt['Area'].sum()
-b=dt['ConvexArea'].sum()
-c=dt['Extent'].sum()
-d=dt['Perimeter'].sum()
-dx=[a,b,c,d]
-cx=pd.DataFrame(dx,index=["Area", "ConvexArea", "Extent","Perimeter"])
-st.bar_chart(cx)
-
-st.write('ค่าเฉลี่ย')
-cl11,cl12,cl13,cl14=st.columns(4)
-cl11.write(dt['Area'].mean())
-cl12.write(dt['ConvexArea'].mean())
-cl13.write(dt['Extent'].mean())
-cl14.write(dt['Perimeter'].mean())
 
 st.write("Area_Chart")
 a=dt['Area'].mean()
@@ -50,3 +31,24 @@ cl21.write(dt['Area'].max())
 cl22.write(dt['ConvexArea'].max())
 cl23.write(dt['Extent'].max())
 cl24.write(dt['Perimeter'].max())
+
+import numpy as np
+import matplotlib.pyplot as plt
+labels = ['Men', 'Women','','']
+sizes = [35,25,15,25]
+explode = (0, 0.1,0,0) 
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+st.pyplot(fig1)
+
+st.write('ค่าน้อยที่สุด')
+cl31,cl32,cl33,cl34=st.columns(4)
+cl31.write(dt['Area'].min())
+cl32.write(dt['ConvexArea'].min())
+cl33.write(dt['Extent'].min())
+cl34.write(dt['Perimeter'].min())
+
+st.write("Line_Chart")
+cc=[3,8,1,10]
+st.line_chart(cc)
