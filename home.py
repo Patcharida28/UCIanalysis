c95f282b18aea2172d586d0e03496776ea95c981
Patcharida28 <patcharidachoomchoo@gmail.com>
2024-03-29 11:42:03 +0700

    Update home.py

--- home.py
+++ home.py
@@ -9,7 +9,7 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-lottie_url_hello = "https://lottie.host/78241b7d-fc55-4aae-9b67-7b6110f85dd3/7Bt8aUYObq.json"
+lottie_url_hello = "https://lottie.host/9d292a79-766e-42e6-81a8-8dec90f5ba59/CiV7jjIgVb.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 st_lottie(lottie_hello,key="hello")
 
