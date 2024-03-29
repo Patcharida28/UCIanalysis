305c1cfc94e34d35bb61d143126b3986d4a01e02
Patcharida28 <patcharidachoomchoo@gmail.com>
2024-03-29 13:36:43 +0700

    Update home.py

--- home.py
+++ home.py
@@ -9,7 +9,7 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 st.title("การประยุกต์ใช้งาน Machine learning บนเว็บ")
-st.subheader(" By นางสาวพัชริดา ชุ่มชู สาขาวิชาวิทยาการคอมพิวเตอร์ คณะวิทยาศาสตร์และเทคโนโลยีมหาวิทยาลัยราชภัฏนครปฐม")
+st.subheader(" By นางสาวพัชริดา ชุ่มชู สาขาวิชาวิทยาการคอมพิวเตอร์ คณะวิทยาศาสตร์และเทคโนโลยี มหาวิทยาลัยราชภัฏนครปฐม")
 
 lottie_url_hello = "https://lottie.host/a4a8e206-d582-4211-9292-e6dbdaf428b1/NbkG79OEyW.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
