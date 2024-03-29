8773f982ec4000401c41fa2e7a807366c5e058d7
Patcharida28 <patcharidachoomchoo@gmail.com>
2024-03-29 12:10:15 +0700

    .

--- pages/1📊Statistic.py
+++ pages/1📊Statistic.py
@@ -6,5 +6,9 @@ import pandas as pd
 
 dt=pd.read_csv('./data/Raisin.csv')
 
+lottie_url_hello = "https://lottie.host/fdd490d8-353e-4c58-a976-abf5f9c99261/mcARPoQsZF.json"
+lottie_hello = load_lottieurl(lottie_url_hello)
+st_lottie(lottie_hello,key="hello")
+
 st.subheader("🍖🍖สถิติข้อมูลแสดงภาพพันธุ์ของลูกเกด🍖🍖")
 st.write(dt.head(50))
--- pages/2📈Chart.py
+++ pages/2📈Chart.py
@@ -13,7 +13,7 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-lottie_url_hello = "https://lottie.host/ee28a6f9-6dec-48f3-9de2-8102599bc0ac/zvxRFHwsnx.json"
+lottie_url_hello = "https://lottie.host/fdd490d8-353e-4c58-a976-abf5f9c99261/mcARPoQsZF.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 st_lottie(lottie_hello,key="hello")
 
